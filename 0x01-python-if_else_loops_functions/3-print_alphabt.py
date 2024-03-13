1407fec7a3797ca5e80fa15daabd76443c4f6891
Motolani Chrsitianah Alebiosu <motolanialebiosu@gmail.com>
2024-03-13 09:07:27 -0700

    printing alphabt

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for char in range(ord("a"), ord("z") + 1):
     if chr(char) == "e" or chr(char) == "q":
-        print(end="")
+        continue
     else:
         print("{:s}".format(chr(char)), end="")
