42556eed3f231ba9eae2ac9c36c2a9c2656cdaf4
Motolani Chrsitianah Alebiosu <motolanialebiosu@gmail.com>
2024-03-13 08:42:28 -0700

    extracting last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,11 +2,15 @@
 import random
 number = random.randint(-10000, 10000)
 number_str = str(number)
-minus = "-" if number < 0 else ""
+exe = 1 if number < 0 else 0
 last_digit = int((number_str[-1:]))
+if exe:
+    number_str = number_str[:-1]
+    number = int(number_str)
+    last_digit *= -1
 if last_digit > 5:
-    print(f"Last digit of {number} is {minus}{last_digit:d} and is greater than 5")
-elif last_digit == "0":
+    print(f"Last digit of {number} is {last_digit} and is greater than 5")
+elif last_digit == 0:
     print(f"Last digit of {number} is {last_digit} and is 0")
 else:
-    print(f"Last digit of {number} is {minus}{last_digit:d} and is less than 6 and not 0")
+    print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
